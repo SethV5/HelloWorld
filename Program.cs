294827bf45c4a5a3c5e6f867526f6e9be507d1dd
SethV5 <33557562+SethV5@users.noreply.github.com>
2017-12-07 14:53:09 -0700

    Commiting Changes From The tutorial Videos.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            // Tell me whats going onin the console as it boots up:
+            //Console.WriteLine("Hello There World!");
+            // Waiting for user input:
+            //Console.ReadLine();
+            // Declaring variable "Input":
+            //string input = Console.ReadLine();
+            // Print to console what i type:
+            //Console.WriteLine(input);
+            // Output this to user:
+            Console.WriteLine("Write Two Numbers");
+            // Made and declared two variables:
+            string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+            // Printing out variable number1:
+            // Console.WriteLine(number1);
+            // Printing out variables: number1 and number2:
+            // Console.WriteLine(number1 + " " + number2);
+
         }
     }
 }
